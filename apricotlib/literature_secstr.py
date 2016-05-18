a48a33a043e3bf3ac5a1bc7b8cc782871cff7c9b
Malvika Sharan <malvikasharan@gmail.com>
2016-05-18 18:48:03 +0200

    Update literature_secstr.py

--- apricotlib/literature_secstr.py
+++ apricotlib/literature_secstr.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python 
 
-'''summarize Literature based secondary structure result'''
-
 import argparse
 import os
 from collections import defaultdict
 
-__description__ = ""
+__description__ = "summarize Literature based secondary structure result"
 __author__ = "Malvika Sharan <malvika.sharan@uni-wuerzburg.de>"
 __email__ = "malvika.sharan@uni-wuerzburg.de"
-__version__ = ""
 
 
 def main():
