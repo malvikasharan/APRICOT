26f575716d428aa5578aa9e4e43e37e820dc069e
Konrad Förstner <konrad@foerstner.org>
2016-05-20 23:57:02 +0200

    Refactoring and reformating

--- run_example.sh
+++ run_example.sh
@@ -1,61 +1,104 @@
 #!/bin/bash
 # AUTHOR: Malvika Sharan <malvikasharan@gmail.com>
 
+#########################################################################
 ANALYSIS_PATH=APRICOT_analysis
 APRICOT_PATH=APRICOT
 ROOT_DB_PATH=source_files
+#########################################################################
+
+
+#########################################################################
+# Download parameters
 ZENODO_LINK_FOR_DEMO_DATA=https://zenodo.org/record/51705/files/APRICOT-1.0-demo_files-MS.zip
 PFAM_RELEASE=Pfam30.0
+#########################################################################
 
+
+#########################################################################
 # FIXED PATHS for flatfiles downloaded by APRICOT
 DB_PATH=$ROOT_DB_PATH/reference_db_files
 
-# PATHS for domain databases
+# Path for domain databases
 CDD_PATH=$DB_PATH/cdd/Cdd
 INTERPRO_PATH=$DB_PATH/interpro/interproscan
-    
-### Inputs: *REQUIRED* or *OPTIONAL* ###
+#########################################################################
 
-# *OPTIONAL* provide species name to retrieve taxonomy ids for given
-# species in bin/selected_taxonomy_ids.txt
-species=''
 
-## *REQUIRED* INPUT-1: query proteins##
+#########################################################################
+# *OPTIONAL* PROVIDE SPECIES NAME TO RETRIEVE TAXONOMY IDS FOR GIVEN
+# SPECIES IN BIN/SELECTED_TAXONOMY_IDS.TXT
+SPECIES=''
+#########################################################################
 
-### This must not be altered by the users in the demonstration file
+
+#######################################################################
+# Input-1, option 1: provide comma separated list of UniProt ids
+#
+# * P0A6X3 (positive test) is Hfq protein that contains sm and RRM/RNP
+#   like domain
+# * P00957 (negative test) is alaS protein is a tRNA-ligase, hence
+#   will not be selected by RRM, KH or DEAD domains
 QUERY_UIDS='P0A6X3,P00957'
-    # Input-1, option 1: provide comma separated list of UniProt ids
-    # P0A6X3 (positive test) is Hfq protein that contains sm and RRM/RNP like domain
-    # P00957 (negative test) is alaS protein is a tRNA-ligase, hence will not be selected by RRM, KH or DEAD domains
-    
-## Not used in the demonstration file  
+#######################################################################
+
+
+######################################################################
+# Not used in this demonstration:
+# Input-1, option 2: provide comma separated list of gene ids or gene
+# name 
 query_geneids=''
-    # Input-1, option 2: provide comma separated list of gene ids or gene name
+
+# Input-1, option 3: users can pick a taxonomy id from option 1a
+# (source_files/selected_taxonomy_ids.txt), or directly provide it
+# when the taxonomy id is known
 tax_id=''
-    # Input-1, option 3: users can pick a taxonomy id from option 1a (source_files/selected_taxonomy_ids.txt), or directly provide it when the taxonomy id is known
+######################################################################
+
+
+######################################################################
+# Input-1, option 4: provide absolute path of for query fasta sequence
+# default fasta path
+# $ANALYSIS_PATH/input/mapped_query_annotation/fasta_path_mapped_query
 FASTA_PATH=$ANALYSIS_PATH/input/mapped_query_annotation/fasta_path_mapped_query
-    # Input-1, option 4: provide absolute path of for query fasta sequence
-    # default fasta path $ANALYSIS_PATH/input/mapped_query_annotation/fasta_path_mapped_query
-    
-### Input-2:  Keywords, *REQUIRED* for domain selection and *OPTIONAL* for classification###
-### This can be altered by the users in the demonstration file as well
-domain_kw='RRM,KH,DEAD'
-    # *REQUIRED* Input-2, comma separated list of keywords for domain selection
-class_kw='ribosom,helicase,synthetase,polymerase,transferase,nuclease,RRM,RNP'
-    ## *OPTIONAL* Input-2, comma separated list of keywords for protein classification based on the predicted domains
+######################################################################
+
+
+######################################################################
+# Input-2: Keywords
+#
+# *REQUIRED* for domain selection
+# *OPTIONAL* for classification
+# This can be altered by the users in the demonstration file as well
+DOMAIN_KEYWORDS='RRM,KH,DEAD'
+######################################################################
+
+
+#######################################################################
+# Input-2, comma separated list of keywords for domain selection
+# *REQUIRED* 
+CLASS_KEYWORDS='ribosom,helicase,synthetase,polymerase,transferase,nuclease,RRM,RNP'
+#######################################################################
+
+
+#######################################################################
+# Input-2, comma separated list of keywords for protein
+# *classification based on the predicted domains
+# *OPTIONAL* 
+#######################################################################
 
 main(){
-    set_up_analysis_folder			# set_up_analysis_folder
-    retrieve_files		                # downloads all the files required for this demo, provide zenodo link
-    provide_input_queries			# subcommand: query
-    provide_domain_and_class_keywords		# subcommand: keywords
-    select_domains_by_keywords			# subcommand: select
-    run_domain_prediction			# subcommand: predict
-    filter_domain_analysis			# subcommand: filter
-    classify_filtered_result			# subcommand: classify
-    calculate_annotation_score			# subcommand: annoscore
-    create_analysis_summary			# subcommand: summary
-    output_file_formats				# subcommand: format
+    set_up_analysis_folder			
+    downloads_files		                
+    provide_input_queries		  # subcommand query
+    provide_domain_and_class_keywords	  # subcommand keywords
+    select_domains_by_keywords	  # subcommand select
+    run_domain_prediction		  # subcommand predict
+    filter_domain_analysis		  # subcommand filter
+    classify_filtered_result		  # subcommand classify
+    calculate_annotation_score	  # subcommand annoscore
+    create_analysis_summary		  # subcommand summary
+    format_output			  # subcommand format
 }
 
 set_up_analysis_folder(){
@@ -67,7 +110,7 @@ set_up_analysis_folder(){
     apricot create $ANALYSIS_PATH
 }
 
-retrieve_files(){
+downloads_files(){
     # Demo files stored at Zenodo
     DEMO_ZIP=$(basename $ZENODO_LINK_FOR_DEMO_DATA)
     DEMO_FOLDER=$(basename $DEMO_ZIP .zip)/apricot_demo_files/
@@ -100,54 +143,54 @@ provide_input_queries(){
 }
 
 provide_domain_and_class_keywords(){
-    python3 $APRICOT_PATH/bin/apricot keywords \
-    $domain_kw \
-    -cl $class_kw
+    apricot keywords \
+	    -cl $CLASS_KEYWORDS \
+	    $DOMAIN_KEYWORDS 
 }
 
 select_domains_by_keywords(){
-    ## Selection of domains from both CDD and InterPro by default
-    ## use from flags -C for CDD or -I for InterPro
-    python3 $APRICOT_PATH/bin/apricot select
+    # Selection of domains from both CDD and InterPro by default.
+    # Use from flags -C for CDD or -I for InterPro
+    apricot select
 }
 
 run_domain_prediction(){
-    ## prediction by both CDD and InterPro by default
-    ## use from flags -C for CDD or -I for InterPro
-    ## use --force or -F option to overwrite the existing analysis
-    python3 $APRICOT_PATH/bin/apricot predict \
-    --analysis_path $ANALYSIS_PATH \
-    --fasta $FASTA_PATH \
-    --cdd_db $CDD_PATH \
-    --ipr_db $INTERPRO_PATH
-    #-F -C -I
+    # Prediction by both CDD and InterPro by default
+    # use from flags -C for CDD or -I for InterPro
+    # use --force or -F option to overwrite the existing analysis
+    apricot predict \
+	    --analysis_path $ANALYSIS_PATH \
+	    --fasta $FASTA_PATH \
+	    --cdd_db $CDD_PATH \
+	    --ipr_db $INTERPRO_PATH
 }
 
 filter_domain_analysis(){
-    python3 $APRICOT_PATH/bin/apricot filter \
-    --analysis_path $ANALYSIS_PATH \
-    --similarity 24 --coverage 39
+    apricot filter \
+	    --analysis_path $ANALYSIS_PATH \
+	    --similarity 24 \
+	    --coverage 39
 }
 
 classify_filtered_result(){
-    python3 $APRICOT_PATH/bin/apricot classify \
-    --analysis_path $ANALYSIS_PATH
+    apricot classify \
+	    --analysis_path $ANALYSIS_PATH
 }
 
 calculate_annotation_score(){
-    python3 $APRICOT_PATH/bin/apricot annoscore \
-    --analysis_path $ANALYSIS_PATH
+    apricot annoscore \
+	    --analysis_path $ANALYSIS_PATH
 }
 
 create_analysis_summary(){
-    python3 $APRICOT_PATH/bin/apricot summary \
-    --analysis_path $ANALYSIS_PATH
+    apricot summary \
+	    --analysis_path $ANALYSIS_PATH
 }
 
-output_file_formats(){
-    python3 $APRICOT_PATH/bin/apricot format \
-    --analysis_path $ANALYSIS_PATH \
-    -HT
+format_output(){
+    apricot format \
+	    --analysis_path $ANALYSIS_PATH \
+	    -HT
 }
 
 main
