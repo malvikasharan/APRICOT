446f78da8695154ad4bbf5d62e587b150dd12380
Konrad Förstner <konrad@foerstner.org>
2016-05-20 19:26:38 +0200

    Cleanup uniprot_taxid.py

--- apricotlib/uniprot_taxid.py
+++ apricotlib/uniprot_taxid.py
@@ -1,20 +1,23 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 
-import os
-import sys
-
-__description__ = "Parses the taxonomy id file from UniProt for all the species."
+__description__ = "Parses the taxonomy id file from UniProt for all" \
+                  " the species."
 __author__ = "Malvika Sharan <malvika.sharan@uni-wuerzburg.de>"
 __email__ = "malvika.sharan@uni-wuerzburg.de"
+
 
 def select_taxids(species, reference_taxonomy_file, selected_taxonomy_file):
     '''Selects taxonomy ids for the query species'''
     if not str(species) == 'None':
-        parse_uniprot_tax_file(species, reference_taxonomy_file, selected_taxonomy_file)
+        parse_uniprot_tax_file(
+            species, reference_taxonomy_file, selected_taxonomy_file)
     else:
-        parse_uniprot_tax_file('N=', reference_taxonomy_file, selected_taxonomy_file)
-    
-def parse_uniprot_tax_file(species, reference_taxonomy_file, selected_taxonomy_file):
+        parse_uniprot_tax_file(
+            'N=', reference_taxonomy_file, selected_taxonomy_file)
+
+        
+def parse_uniprot_tax_file(
+        species, reference_taxonomy_file, selected_taxonomy_file):
     '''Parses the taxonomy id file from UniProt for all the species'''
     with open(selected_taxonomy_file, 'w') as out_fh:
         out_fh.write("Official (scientific) name\tTaxonomy ID\n")
@@ -26,15 +29,10 @@ def parse_uniprot_tax_file(species, reference_taxonomy_file, selected_taxonomy_f
                             if each_species.lower() in entry.lower():
                                 tax_id = entry.split(':')[0].split(' ')[-1]
                                 species_name = entry.strip().split('=')[1]
-                                out_fh.write("%s\t%s\n" % (species_name, tax_id))
+                                out_fh.write("%s\t%s\n" % (
+                                    species_name, tax_id))
                     else:
                         if species.lower() in entry.lower():
                             tax_id = entry.split(':')[0].split(' ')[-1]
                             species_name = entry.strip().split('=')[1]
                             out_fh.write("%s\t%s\n" % (species_name, tax_id))
-  
-if __name__ == '__main__':
-    species = sys.argv[1]
-    reference_taxonomy_file = sys.argv[2]
-    selected_taxonomy_file = sys.argv[3]
-    setup_analysis_folders(species, reference_taxonomy_file, selected_taxonomy_file)
