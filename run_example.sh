4d56d9038b279e4a2bc0bd5843aaecfbe0acc185
Konrad Förstner <konrad@foerstner.org>
2016-05-20 23:08:29 +0200

    Refactoring

--- run_example.sh
+++ run_example.sh
@@ -1,33 +1,29 @@
 #!/bin/bash
-#AUTHOR: Malvika Sharan <malvikasharan@gmail.com>
+# AUTHOR: Malvika Sharan <malvikasharan@gmail.com>
 
-PYTHON_PATH=python3
 ANALYSIS_PATH=APRICOT_analysis
 APRICOT_PATH=APRICOT
 ROOT_DB_PATH=source_files
+ZENODO_LINK_FOR_DEMO_DATA=https://zenodo.org/record/51705/files/APRICOT-1.0-demo_files-MS.zip
+PFAM_RELEASE=Pfam30.0
 
-## FIXED PATHS for flatfiles downloaded by APRICOT
+# FIXED PATHS for flatfiles downloaded by APRICOT
 DB_PATH=$ROOT_DB_PATH/reference_db_files
 
-## PATHS for domain databases
+# PATHS for domain databases
 CDD_PATH=$DB_PATH/cdd/Cdd
 INTERPRO_PATH=$DB_PATH/interpro/interproscan
-
-if ! [ -e $APRICOT_PATH/apricotlib ]
-then
-    git clone https://github.com/malvikasharan/APRICOT.git $APRICOT_PATH
-    ln -s $APRICOT_PATH/apricotlib $APRICOT_PATH/bin
-fi
     
 ### Inputs: *REQUIRED* or *OPTIONAL* ###
 
-## *OPTIONAL* provide species name to retrieve taxonomy ids for given species in bin/selected_taxonomy_ids.txt##
+# *OPTIONAL* provide species name to retrieve taxonomy ids for given
+# species in bin/selected_taxonomy_ids.txt
 species=''
 
 ## *REQUIRED* INPUT-1: query proteins##
 
 ### This must not be altered by the users in the demonstration file
-query_uids='P0A6X3,P00957'
+QUERY_UIDS='P0A6X3,P00957'
     # Input-1, option 1: provide comma separated list of UniProt ids
     # P0A6X3 (positive test) is Hfq protein that contains sm and RRM/RNP like domain
     # P00957 (negative test) is alaS protein is a tRNA-ligase, hence will not be selected by RRM, KH or DEAD domains
@@ -50,7 +46,7 @@ class_kw='ribosom,helicase,synthetase,polymerase,transferase,nuclease,RRM,RNP'
 
 main(){
     set_up_analysis_folder			# set_up_analysis_folder
-    basic_requirements_for_demo			# downloads all the files required for this demo, provide zenodo link
+    retrieve_files		                # downloads all the files required for this demo, provide zenodo link
     provide_input_queries			# subcommand: query
     provide_domain_and_class_keywords		# subcommand: keywords
     select_domains_by_keywords			# subcommand: select
@@ -63,52 +59,44 @@ main(){
 }
 
 set_up_analysis_folder(){
-    
-    for paths in $ROOT_DB_PATH $APRICOT_PATH $ANALYSIS_PATH $APRICOT_LIBRARY
+    mkdir -p $ROOT_DB_PATH $APRICOT_PATH $ANALYSIS_PATH $APRICOT_LIBRARY
+    for DB_SUBPATH in cdd go_mapping interpro pfam
     do
-        if ! [ -d $paths ] 
-        then
-            mkdir $paths
-        fi
+	mkdir -p $DB_PATH/$DB_SUBPATH
     done
-    if ! [ -d $DB_PATH ] 
-        then
-            mkdir $DB_PATH
-    fi
-    for db_subpath in cdd go_mapping interpro pfam
-    do
-        if ! [ -d $DB_PATH/$db_subpath ] 
-        then
-            mkdir $DB_PATH/$db_subpath
-        fi
-    done
-    
-    python3 $APRICOT_PATH/bin/apricot create \
-    $ANALYSIS_PATH
+    apricot create $ANALYSIS_PATH
 }
 
-basic_requirements_for_demo(){
-    #zenodo_link_for_demo_data=https://zenodo.org/record/51705/files/APRICOT-1.0-demo_files-MS.zip
-    #wget $zenodo_link_for_demo_data
-    #unzip apricot_demo_files.zip
-    cp apricot_demo_files/go_mapping/* $DB_PATH/go_mapping
-    cp -r apricot_demo_files/interpro_annotation_data $DB_PATH/interpro
-    cp apricot_demo_files/cdd_analysis/* $ANALYSIS_PATH/output/0_predicted_domains/cdd_analysis
-    cp apricot_demo_files/ipr_analysis/* $ANALYSIS_PATH/output/0_predicted_domains/ipr_analysis
-    ## CDD annotation table
-    wget -c -P $DB_PATH/cdd/cdd_annotation_data ftp://ftp.ncbi.nih.gov/pub/mmdb/cdd/cddid.tbl.gz
+retrieve_files(){
+    # Demo files stored at Zenodo
+    DEMO_ZIP=$(basename $ZENODO_LINK_FOR_DEMO_DATA)
+    DEMO_FOLDER=$(basename $DEMO_ZIP .zip)/apricot_demo_files/
+    wget $ZENODO_LINK_FOR_DEMO_DATA
+    unzip ${DEMO_ZIP}
+    cp ${DEMO_FOLDER}/go_mapping/* $DB_PATH/go_mapping
+    cp -r ${DEMO_FOLDER}/interpro_annotation_data $DB_PATH/interpro
+    cp ${DEMO_FOLDER}/cdd_analysis/* $ANALYSIS_PATH/output/0_predicted_domains/cdd_analysis
+    cp ${DEMO_FOLDER}/ipr_analysis/* $ANALYSIS_PATH/output/0_predicted_domains/ipr_analysis
+    rm -rf $DEMO_ZIP $DEMO_FOLDER $(basename $DEMO_ZIP .zip)
+
+    # CDD annotation table
+    wget -c \
+	 -P $DB_PATH/cdd/cdd_annotation_data \
+	 ftp://ftp.ncbi.nih.gov/pub/mmdb/cdd/cddid.tbl.gz
     gunzip $DB_PATH/cdd/cdd_annotation_data/*
-    ## PfamA annotation table
-    pfam_release=Pfam30.0
-    wget -c -P $DB_PATH/pfam ftp://ftp.ebi.ac.uk/pub/databases/Pfam/releases/$pfam_release/database_files/pfamA.txt.gz
-    gunzip $DB_PATH'/pfam/pfamA.txt.gz'
+    
+    # PfamA annotation table
+    wget -c \
+	 -P $DB_PATH/pfam \
+	 ftp://ftp.ebi.ac.uk/pub/databases/Pfam/releases/$PFAM_RELEASE/database_files/pfamA.txt.gz
+    gunzip $DB_PATH/pfam/pfamA.txt.gz
 }
 
 provide_input_queries(){
-    ## Option-1: UniProt identifiers
-    python3 $APRICOT_PATH/bin/apricot query \
-    --analysis_path $ANALYSIS_PATH \
-    --uids $query_uids
+    # Option-1: UniProt identifiers
+    apricot query \
+	    --analysis_path $ANALYSIS_PATH \
+	    --uids $QUERY_UIDS
 }
 
 provide_domain_and_class_keywords(){
