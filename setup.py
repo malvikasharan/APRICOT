f81097d64dd334fb0f04063e9190784cf46eb44e
Konrad Förstner <konrad@foerstner.org>
2016-05-21 14:17:43 +0200

    Clean

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ except ImportError:
 
 setup(name='bio-APRICOT',
       version='1.0.2',
-      description='Sequence-based identification and characterization of protein classes',
+      description=('Sequence-based identification and characterization '
+                   'of protein classes'),
       author='Malvika Sharan',
       author_email='malvikasharan@gmail.com',
       url='https://www.python.org/pypi/bio-apricot',
@@ -19,14 +20,13 @@ setup(name='bio-APRICOT',
         "openpyxl >= 2.3.1",
         "numpy >= 1.9.0",
         "scipy >= 0.16.0"
-    ],
-    scripts=['bin/apricot'],
-    license='ISC License (ISCL)',
-    long_description=open('README.rst').read(),
-    classifiers=[
-        'License :: OSI Approved :: ISC License (ISCL)',
-        'Operating System :: POSIX',
-        'Programming Language :: Python :: 3',
-        'Topic :: Scientific/Engineering :: Bio-Informatics',
-    ]
-)   
+      ],
+      scripts=['bin/apricot'],
+      license='ISC License (ISCL)',
+      long_description=open('README.rst').read(),
+      classifiers=[
+          'License :: OSI Approved :: ISC License (ISCL)',
+          'Operating System :: POSIX',
+          'Programming Language :: Python :: 3',
+          'Topic :: Scientific/Engineering :: Bio-Informatics',
+      ])
