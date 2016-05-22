a047b0a3e17c3726d82ce973685546c36b27139f
Konrad Förstner <konrad@foerstner.org>
2016-05-22 19:16:08 +0200

    Fix variable style

--- shell_scripts/setup_pdb_data.sh
+++ shell_scripts/setup_pdb_data.sh
@@ -5,12 +5,12 @@
 
 PYTHON_PATH=python
 
-apricot_path=$1
+APRICOT_PATH=$1
 ## full path where you have cloned/save APRICOT from git
-apricot_lib=$apricot_path/apricotlib
+apricot_lib=$APRICOT_PATH/apricotlib
 
 ## path where you want to store the data
-apricot_db=$2
+APRICOT_DB_PATH=$2
 
 main(){
         create_main_path
@@ -23,38 +23,38 @@ main(){
 }
 
 create_main_path(){
-    if ! [ -d $apricot_db ]
+    if ! [ -d $APRICOT_DB_PATH ]
         then
-            mkdir -p $apricot_db
+            mkdir -p $APRICOT_DB_PATH
     fi
-    if ! [ -d $apricot_db/pdb ]
+    if ! [ -d $APRICOT_DB_PATH/pdb ]
     then
-        mkdir -p $apricot_db/pdb
+        mkdir -p $APRICOT_DB_PATH/pdb
     fi
 }
 
 create_pdb_inpath(){
     for FOLDER in pdb_sequence pdb_secstr pdb2uniprot
     do
-        if ! [ -d $apricot_db/pdb/$FOLDER ]
+        if ! [ -d $APRICOT_DB_PATH/pdb/$FOLDER ]
         then
-            mkdir -p $apricot_db/pdb/$FOLDER
+            mkdir -p $APRICOT_DB_PATH/pdb/$FOLDER
         fi
     done
 }
 
 download_uniprot_map(){
-    wget -P $apricot_db/pdb/pdb2uniprot http://www.uniprot.org/docs/pdbtosp.txt
+    wget -P $APRICOT_DB_PATH/pdb/pdb2uniprot http://www.uniprot.org/docs/pdbtosp.txt
 }
 
 download_pdb_sec_str(){
-    wget -c -P $apricot_db/pdb/pdb_secstr http://www.rcsb.org/pdb/files/ss.txt
+    wget -c -P $APRICOT_DB_PATH/pdb/pdb_secstr http://www.rcsb.org/pdb/files/ss.txt
 }
 
 sort_and_format_pdb_data(){
     $PYTHON_PATH $apricot_lib/sort_sec_struc_pdb_data.py \
-    -i $apricot_db/pdb/pdb_secstr/ss.txt -seq $apricot_db/pdb/pdb_sequence -str $apricot_db/pdb/pdb_secstr
-    $apricot_db/blast/makeblastdb -in $apricot_db/pdb/pdb_sequence/pdb_sequence.txt -dbtype prot
+    -i $APRICOT_DB_PATH/pdb/pdb_secstr/ss.txt -seq $APRICOT_DB_PATH/pdb/pdb_sequence -str $APRICOT_DB_PATH/pdb/pdb_secstr
+    $APRICOT_DB_PATH/blast/makeblastdb -in $APRICOT_DB_PATH/pdb/pdb_sequence/pdb_sequence.txt -dbtype prot
 }
 
 main
