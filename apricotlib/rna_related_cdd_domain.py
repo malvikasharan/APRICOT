4c17e8b6ee535ebf621dcde43774dbc1d8688199
Malvika Sharan <malvikasharan@gmail.com>
2016-05-23 23:39:49 +0200

    Update rna_related_cdd_domain.py

--- apricotlib/rna_related_cdd_domain.py
+++ apricotlib/rna_related_cdd_domain.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+# Description = RNA related CDD domain
+# author= "Malvika Sharan <malvika.sharan@uni-wuerzburg.de>"
+# email = "malvika.sharan@uni-wuerzburg.de"
+# 2016-05-20
 
 import sys
 import re
