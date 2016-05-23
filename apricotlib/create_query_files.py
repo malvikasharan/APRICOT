60eb74604944e9f247ea5a04031d4f3b3cb35834
Malvika Sharan <malvikasharan@gmail.com>
2016-05-23 23:06:10 +0200

    Update create_query_files.py

--- apricotlib/create_query_files.py
+++ apricotlib/create_query_files.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+# Description = Prepares query protein data for analysis
+# author= "Malvika Sharan <malvika.sharan@uni-wuerzburg.de>"
+# email = "malvika.sharan@uni-wuerzburg.de"
+# 2016-05-20
 
 
 def create_uid_query_file(uids, query_path):
