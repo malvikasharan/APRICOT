19ac130f164a97a9a2da321c8a97094384936c5f
Malvika Sharan <malvikasharan@gmail.com>
2016-05-23 23:29:06 +0200

    Update map_domain_to_go.py

--- apricotlib/map_domain_to_go.py
+++ apricotlib/map_domain_to_go.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python 
+# Description = Maps each domains to their corresponding domains from InterPro
+# author= "Malvika Sharan <malvika.sharan@uni-wuerzburg.de>"
+# email = "malvika.sharan@uni-wuerzburg.de"
+# 2016-05-20
 
 import argparse
 import os
 
 __description__ = "Maps each domains to their corresponding domains from InterPro"
-__author__ = "Malvika Sharan <malvika.sharan@uni-wuerzburg.de>"
-__email__ = "malvika.sharan@uni-wuerzburg.de"
 
 
 def main():
