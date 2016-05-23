92cd02e37adb7854195998f5fd4d1813e37e78e7
Malvika Sharan <malvikasharan@gmail.com>
2016-05-23 23:28:07 +0200

    Update list_similar_pdbstr.py

--- apricotlib/list_similar_pdbstr.py
+++ apricotlib/list_similar_pdbstr.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+# Description = Identifies similar PDB structures based on secondary structures
+# author= "Malvika Sharan <malvika.sharan@uni-wuerzburg.de>"
+# email = "malvika.sharan@uni-wuerzburg.de"
+# 2016-05-20
 
 import os
 import sys
