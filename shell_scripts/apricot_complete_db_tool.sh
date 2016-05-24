b0b28bbeb4425017c6b310b94cbfd8a202d1b503
Malvika Sharan <malvikasharan@gmail.com>
2016-05-24 10:21:35 +0200

    Update apricot_complete_db_tool.sh

--- shell_scripts/apricot_complete_db_tool.sh
+++ shell_scripts/apricot_complete_db_tool.sh
@@ -3,9 +3,8 @@
 
 PYTHON_PATH=python
 apricot_path=$1
-apricot_lib= ../$apricot_path/apricotlib   ## provide full path where you have cloned/save APRICOT from git
-apricot_db=$2                           ## apricot_db_path ##provide full path where you want to store the data
-apricot_flatfiles=bin/reference_db_files/
+apricot_lib= ../$apricot_path/apricotlib        ## provide full path where you have cloned/save APRICOT from git
+apricot_db=source_files/reference_db_files      ## apricot_db_path ##provide full path where you want to store the data
 
 main(){
         create_db_path
@@ -158,15 +157,11 @@ install_psortb(){
 }
 
 create_flatfile_path(){
-    if ! [ -d $apricot_flatfiles ]
-        then
-            mkdir -p $apricot_flatfiles
-    fi
     for FOLDER in cdd interpro blast go_mapping biojs pdb all_taxids
     do
-        if ! [ -d $apricot_flatfiles/$FOLDER ]
+        if ! [ -d $apricot_db/$FOLDER ]
         then
-            mkdir -p $apricot_flatfiles/$FOLDER
+            mkdir -p $apricot_db/$FOLDER
         fi
     done
 }
@@ -174,78 +169,78 @@ create_flatfile_path(){
 create_cdd_flatfiles_inpath(){
     for FOLDER in version_info cdd_annotation_data cdd_flat_files
     do
-        if ! [ -d $apricot_flatfiles/cdd/$FOLDER ]
+        if ! [ -d $apricot_db/cdd/$FOLDER ]
         then
-            mkdir -p $apricot_flatfiles/cdd/$FOLDER
+            mkdir -p $apricot_db/cdd/$FOLDER
         fi
     done
 }
 
 check_and_download_cdd_flatfiles(){
-    rm -rf $apricot_flatfiles/cdd/cdd_flat_files/*
-    wget -c -P $apricot_flatfiles/cdd/ ftp://ftp.ncbi.nih.gov/pub/mmdb/cdd/cdd.info
-    if diff $apricot_flatfiles/cdd/cdd.info $apricot_flatfiles/cdd/version_info/cdd.info >/dev/null ; then
-        echo '\n**'updated vesion of cdd is present in path $apricot_flatfiles/cdd.'**\n'
+    rm -rf $apricot_db/cdd/cdd_flat_files/*
+    wget -c -P $apricot_db/cdd/ ftp://ftp.ncbi.nih.gov/pub/mmdb/cdd/cdd.info
+    if diff $apricot_db/cdd/cdd.info $apricot_db/cdd/version_info/cdd.info >/dev/null ; then
+        echo '\n**'updated vesion of cdd is present in path $apricot_db/cdd.'**\n'
     fi
-    cp $apricot_flatfiles/cdd/cdd.info $apricot_flatfiles/cdd/version_info/cdd.info
-    rm $apricot_flatfiles/cdd/cdd_annotation_data/*
-    wget -c -P $apricot_flatfiles/cdd/cdd_annotation_data ftp://ftp.ncbi.nih.gov/pub/mmdb/cdd/cddid.tbl.gz
-    gunzip $apricot_flatfiles/cdd/cdd_annotation_data/*
+    cp $apricot_db/cdd/cdd.info $apricot_db/cdd/version_info/cdd.info
+    rm $apricot_db/cdd/cdd_annotation_data/*
+    wget -c -P $apricot_db/cdd/cdd_annotation_data ftp://ftp.ncbi.nih.gov/pub/mmdb/cdd/cddid.tbl.gz
+    gunzip $apricot_db/cdd/cdd_annotation_data/*
 }
 
 create_interpro_flatfiles_inpath(){
     for FOLDER in version_info ipr_annotation_data interpro_flat_files
     do
-        if ! [ -d $apricot_flatfiles/interpro/$FOLDER ]
+        if ! [ -d $apricot_db/interpro/$FOLDER ]
         then
-            mkdir -p $apricot_flatfiles/interpro/$FOLDER
+            mkdir -p $apricot_db/interpro/$FOLDER
         fi
     done
 }
 
 check_and_download_interpro_flatfiles(){
     ftp_server=ftp://ftp.ebi.ac.uk/pub/databases/interpro
-    wget -O - ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/ > $apricot_flatfiles/interpro/ipr_info.html
-    if diff $apricot_flatfiles/interpro/ipr_info.html $apricot_flatfiles/interpro/version_info/ipr_info.html >/dev/null ; then
-  	echo '\n**'updated vesion of InterPro is present in path $apricot_flatfiles/interpro.'**\n'
+    wget -O - ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/ > $apricot_db/interpro/ipr_info.html
+    if diff $apricot_db/interpro/ipr_info.html $apricot_db/interpro/version_info/ipr_info.html >/dev/null ; then
+  	echo '\n**'updated vesion of InterPro is present in path $apricot_db/interpro.'**\n'
     fi
-    cp $apricot_flatfiles/interpro/ipr_info.html $apricot_flatfiles/interpro/version_info/ipr_info.html
-    rm -rf $apricot_flatfiles/interpro/interpro_flat_files/*
-    wget -O - $ftp_server/ > $apricot_flatfiles/interpro/version_info/ipr_flatfile.html
-    $PYTHON_PATH $apricot_lib/current_interpro_version.py $apricot_flatfiles/interpro/version_info/ipr_flatfile.html $apricot_flatfiles/interpro/ipr_flatfile
-    ipr_flatfile=$(cat $apricot_flatfiles/interpro/ipr_flatfile)
-    wget -c -P $apricot_flatfiles/interpro/ipr_annotation_data $ftp_server/$ipr_flatfile/interpro2go
-    wget -c -P $apricot_flatfiles/interpro/ipr_annotation_data $ftp_server/interpro.xml.gz
-    gunzip $apricot_flatfiles/interpro/ipr_annotation_data/interpro.xml.gz
-    $PYTHON_PATH $apricot_lib/interpro_xml_to_table.py $apricot_flatfiles/interpro/ipr_annotation_data $apricot_flatfiles/interpro/ipr_annotation_data/interpro.xml
+    cp $apricot_db/interpro/ipr_info.html $apricot_db/interpro/version_info/ipr_info.html
+    rm -rf $apricot_db/interpro/interpro_flat_files/*
+    wget -O - $ftp_server/ > $apricot_db/interpro/version_info/ipr_flatfile.html
+    $PYTHON_PATH $apricot_lib/current_interpro_version.py $apricot_db/interpro/version_info/ipr_flatfile.html $apricot_db/interpro/ipr_flatfile
+    ipr_flatfile=$(cat $apricot_db/interpro/ipr_flatfile)
+    wget -c -P $apricot_db/interpro/ipr_annotation_data $ftp_server/$ipr_flatfile/interpro2go
+    wget -c -P $apricot_db/interpro/ipr_annotation_data $ftp_server/interpro.xml.gz
+    gunzip $apricot_db/interpro/ipr_annotation_data/interpro.xml.gz
+    $PYTHON_PATH $apricot_lib/interpro_xml_to_table.py $apricot_db/interpro/ipr_annotation_data $apricot_db/interpro/ipr_annotation_data/interpro.xml
 }
 
 uniprot_species_to_taxid(){
-    wget -c -P $apricot_flatfiles/all_taxids http://www.uniprot.org/docs/speclist.txt
+    wget -c -P $apricot_db/all_taxids http://www.uniprot.org/docs/speclist.txt
 }
 
 create_pdb_inpath(){
     for FOLDER in pdb_sequence pdb_secstr pdb2uniprot
     do
-        if ! [ -d $apricot_flatfiles/pdb/$FOLDER ]
+        if ! [ -d $apricot_db/pdb/$FOLDER ]
         then
-            mkdir -p $apricot_flatfiles/pdb/$FOLDER
+            mkdir -p $apricot_db/pdb/$FOLDER
         fi
     done
 }
 
 download_uniprot_map(){
-    wget -P $apricot_flatfiles/pdb/pdb2uniprot http://www.uniprot.org/docs/pdbtosp.txt
+    wget -P $apricot_db/pdb/pdb2uniprot http://www.uniprot.org/docs/pdbtosp.txt
 }
 
 download_pdb_sec_str(){
-    wget -c -P $apricot_flatfiles/pdb/pdb_secstr http://www.rcsb.org/pdb/files/ss.txt
+    wget -c -P $apricot_db/pdb/pdb_secstr http://www.rcsb.org/pdb/files/ss.txt
 }
 
 sort_and_format_pdb_data(){
     $PYTHON_PATH $apricot_lib/sort_sec_struc_pdb_data.py \
-    -i $apricot_flatfiles/pdb/pdb_secstr/ss.txt -seq $apricot_flatfiles/pdb/pdb_sequence -str $apricot_flatfiles/pdb/pdb_secstr
-    $apricot_flatfiles/blast/makeblastdb -in $apricot_flatfiles/pdb/pdb_sequence/pdb_sequence.txt -dbtype prot
+    -i $apricot_db/pdb/pdb_secstr/ss.txt -seq $apricot_db/pdb/pdb_sequence -str $apricot_db/pdb/pdb_secstr
+    $apricot_db/blast/makeblastdb -in $apricot_db/pdb/pdb_sequence/pdb_sequence.txt -dbtype prot
 }
 
 get_pfam_domain_file(){
@@ -257,54 +252,54 @@ get_pfam_domain_file(){
 }
 
 get_blast_executables(){
-    wget -c -P $apricot_flatfiles/blast ftp://ftp.ncbi.nih.gov/blast/executables/blast+/2.2.28/*x64-linux*
-    tar -xvzf $apricot_flatfiles/blast/*x64-linux* -C $apricot_flatfiles/blast
-    rm -rf $apricot_flatfiles/blast/*x64-linux*.gz
-    for exepath in $(ls $apricot_flatfiles/blast)
+    wget -c -P $apricot_db/blast ftp://ftp.ncbi.nih.gov/blast/executables/blast+/2.2.28/*x64-linux*
+    tar -xvzf $apricot_db/blast/*x64-linux* -C $apricot_db/blast
+    rm -rf $apricot_db/blast/*x64-linux*.gz
+    for exepath in $(ls $apricot_db/blast)
     do
-        if [ ! -p $apricot_flatfiles/blast/makeblastdb ]
+        if [ ! -p $apricot_db/blast/makeblastdb ]
         then
             if [ 'ncbi' = $(echo $exepath | cut -d \- -f 1) ]
             then
-                cp -r $apricot_flatfiles/blast/$exepath/* $apricot_flatfiles/blast
+                cp -r $apricot_db/blast/$exepath/* $apricot_db/blast
             fi
         fi
-        install $apricot_flatfiles/blast/bin/psiblast $apricot_flatfiles/blast
-        install $apricot_flatfiles/blast/bin/blastp $apricot_flatfiles/blast
-        install $apricot_flatfiles/blast/bin/makeblastdb $apricot_flatfiles/blast
+        install $apricot_db/blast/bin/psiblast $apricot_db/blast
+        install $apricot_db/blast/bin/blastp $apricot_db/blast
+        install $apricot_db/blast/bin/makeblastdb $apricot_db/blast
     done
 }
 
 ontology_mapping_to_domains(){
-    GO_PATH=$apricot_flatfiles/go_mapping
+    GO_PATH=$apricot_db/go_mapping
     wget -P $GO_PATH http://www.geneontology.org/ontology/go.obo
-    interpro_to_go=$apricot_flatfiles/interpro/ipr_annotation_data/interpro2go
-    interpro_data=$apricot_flatfiles/interpro/ipr_annotation_data/interproid.tbl
-    cdd_data=$apricot_flatfiles/cdd/cdd_annotation_data/cddid.tbl
+    interpro_to_go=$apricot_db/interpro/ipr_annotation_data/interpro2go
+    interpro_data=$apricot_db/interpro/ipr_annotation_data/interproid.tbl
+    cdd_data=$apricot_db/cdd/cdd_annotation_data/cddid.tbl
     $PYTHON_PATH $apricot_lib/map_domain_to_go.py $GO_PATH $interpro_to_go $interpro_data $cdd_data
 }
 
 get_biojs_dependencies(){
     for module in sequence protein msa
     do
-        if ! [ -d  $apricot_flatfiles/biojs/$module ]
+        if ! [ -d  $apricot_db/biojs/$module ]
         then
-            mkdir $apricot_flatfiles/biojs/$module
+            mkdir $apricot_db/biojs/$module
         fi
     done
     npm install -g mkdirp
     npm install -g browserify
-    npm install biojs-vis-protein-viewer --prefix $apricot_flatfiles/biojs/protein
-    npm install biojs-vis-sequence --prefix $apricot_flatfiles/biojs/sequence
-    npm install msa --prefix $apricot_flatfiles/biojs/msa
+    npm install biojs-vis-protein-viewer --prefix $apricot_db/biojs/protein
+    npm install biojs-vis-sequence --prefix $apricot_db/biojs/sequence
+    npm install msa --prefix $apricot_db/biojs/msa
 }
 
 get_clustalw(){
-    wget -P $apricot_flatfiles/clustal ftp://ftp.ebi.ac.uk/pub/software/clustalw2/2.0.12/clustalw-2.0.12-linux-i686-libcppstatic.tar.gz
-    tar -xvzf $apricot_flatfiles/clustal/clustalw-2.0.12-linux-i686-libcppstatic.tar.gz -C $apricot_flatfiles/clustal
-    rm -rf $apricot_flatfiles/clustal/clustalw-2.0.12-linux-i686-libcppstatic.tar.gz
-    mv $apricot_flatfiles/clustal/clustalw*/* $apricot_flatfiles/clustal
-    rm -rf $apricot_flatfiles/clustal/clustalw-2.0.12-linux-i686-libcppstatic
+    wget -P $apricot_db/clustal ftp://ftp.ebi.ac.uk/pub/software/clustalw2/2.0.12/clustalw-2.0.12-linux-i686-libcppstatic.tar.gz
+    tar -xvzf $apricot_db/clustal/clustalw-2.0.12-linux-i686-libcppstatic.tar.gz -C $apricot_db/clustal
+    rm -rf $apricot_db/clustal/clustalw-2.0.12-linux-i686-libcppstatic.tar.gz
+    mv $apricot_db/clustal/clustalw*/* $apricot_db/clustal
+    rm -rf $apricot_db/clustal/clustalw-2.0.12-linux-i686-libcppstatic
 }
 
 main
