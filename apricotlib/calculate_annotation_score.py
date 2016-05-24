48f3fe6a187498852cbfba37b1876d71a61bc113
Malvika Sharan <malvikasharan@gmail.com>
2016-05-24 09:23:46 +0200

    Update calculate_annotation_score.py

--- apricotlib/calculate_annotation_score.py
+++ apricotlib/calculate_annotation_score.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # Description = Calculate feature based combined annotation scores
 # author= "Malvika Sharan <malvika.sharan@uni-wuerzburg.de>"
-# email = "malvika.sharan@uni-wuerzburg.de"
-# 2016-05-20
 
 import os
 import sys
