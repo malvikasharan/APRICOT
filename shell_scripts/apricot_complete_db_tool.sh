73e4f020c493a8d901da9994cd27502239d076cb
Malvika Sharan <malvikasharan@gmail.com>
2016-05-30 18:01:06 +0200

    Update apricot_complete_db_tool.sh

--- shell_scripts/apricot_complete_db_tool.sh
+++ shell_scripts/apricot_complete_db_tool.sh
@@ -35,7 +35,7 @@ main(){
         ### Other-requirements ###
         get_pfam_domain_file
         get_blast_executables
-        #get_emboss
+        get_emboss
         ontology_mapping_to_domains
         get_biojs_dependencies
         get_clustalw
@@ -273,9 +273,10 @@ get_blast_executables(){
 
 get_emboss(){
     wget -P $apricot_files/blast ftp://emboss.open-bio.org/pub/EMBOSS/old/6.5.0/emboss-latest.tar.gz
-    tar -xvzf $apricot_files/blast/emboss-latest.tar.gz -C $apricot_files/blast
-    ./$apricot_files/blast/EMBOSS*/configure
-    rm config.log 
+    tar -xvzf $apricot_files/needle/emboss-latest.tar.gz -C $apricot_files/needle
+    mv $apricot_files/needle/EMBOSS*/* $apricot_files/needle
+    cd $apricot_files/needle && ./configure && make && cd -
+    echo "In order to re-install please delete (rm) config.log file from your present working directory"
 }
 
 ontology_mapping_to_domains(){
