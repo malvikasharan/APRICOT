0451d9351b092dff7709e1cd67376125f244e477
Malvika Sharan <malvikasharan@gmail.com>
2016-06-17 10:45:27 +0200

    Update docker_support.sh

--- shell_scripts/docker_support.sh
+++ shell_scripts/docker_support.sh
@@ -8,16 +8,11 @@ apricot_lib=APRICOT/apricotlib
 
 ## path for apricot source files
 apricot_files=source_files/reference_db_files
-NR_PATH=source_files/reference_db_files/nr
 
 main(){
         format_interpro_table
         ontology_mapping_to_domains
         sort_and_format_pdb_data
-        
-        ## Uncomment the line below to install RaptorX locally
-        ## Requires locally installed nr database
-        # install_raptorx
 }
 
 format_interpro_table(){
@@ -38,17 +33,6 @@ ontology_mapping_to_domains(){
     interpro_data=$apricot_files/interpro/interpro_annotation_data/interproid.tbl
     cdd_data=$apricot_files/cdd/cdd_annotation_data/cddid.tbl
     python $apricot_lib/map_domain_to_go.py $GO_PATH $interpro_to_go $interpro_data $cdd_data
-}
-
-install_raptorx(){
-        echo "Make sure that you have locally stored RaptorX software and nr database using Dockerfile"
-        echo "Optionally, run the following lines to obtain RaptorX manually:"
-        " wget -c -P source_files/reference_db_files/raptorx http://ttic.uchicago.edu/~zywang/RaptorX-SS8/raptorx-ss8-0.1.tgz"
-        " tar -xvzf source_files/reference_db_files/raptorx/raptorx-ss8-0.1.tgz -C source_files/reference_db_files/raptorx"
-        
-        RUN perl source_files/reference_db_files/raptorx/raptorx-ss3-ss8/setup.pl \
-        -home source_files/reference_db_files/raptorx/raptorx-ss3-ss8 \
-        -blast source_files/reference_db_files/blast/psiblast -nr $NR_PATH/nr
 }
 
 main
