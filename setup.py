2b6c856a603c0797ade662f2cbb24f206cc62497
Malvika Sharan <malvika@wcvh001.rzhousing.uni-wuerzburg.de>
2016-06-20 17:00:42 +0200

    Edited documents for release version 1.1.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='bio-APRICOT',
-      version='1.1.4',
+      version='1.1.5',
       description=('Sequence-based identification and characterization '
                    'of protein classes'),
       author='Malvika Sharan',
